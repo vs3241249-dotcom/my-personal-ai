d488759ac53e38ff6d5635d31afb721d271c78c2
vs3241249-dotcom <vs3241249@gmail.com>
2026-02-11 14:41:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,45 @@ app.get("/dashboard", (req, res) => {
 
 
 // server
-app.listen(3000, () => {
-  console.log("🚀 Auth server running on http://localhost:3000");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ Mongo error", err));
+
+// auth routes
+app.use("/auth", authRoutes);
+
+// static files
+app.use("/static", express.static(path.join(__dirname, "../static")));
+
+// pages
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../templates", "admin_login.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../templates", "admin_login.html"));
+});
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "../templates", "admin_dashboard.html"));
+});
+
+
+// server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
