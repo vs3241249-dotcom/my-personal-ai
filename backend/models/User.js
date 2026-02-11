b5e0e4c3b11ccdfdef9c0c545108c9cf341c0a71
vs3241249-dotcom <vs3241249@gmail.com>
2026-02-11 15:35:03 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,18 +6,9 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
   password: {
     type: String,
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 });
 
