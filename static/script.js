1d44bb025da3d36094cfa1e18657bc42b9037305
vs3241249-dotcom <vs3241249@gmail.com>
2026-02-11 14:55:29 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -258,7 +258,7 @@ loginBtn.addEventListener("click", async () => {
   }
 
   try {
-    const response = await fetch("/login", {
+    const response = await fetch("/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: u, password: p })
