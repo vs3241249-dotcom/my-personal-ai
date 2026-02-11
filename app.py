1a5d2b55250777dc038ef18b70e9a45555d71e06
vs3241249-dotcom <vs3241249@gmail.com>
2026-02-11 20:48:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,7 @@ def export_csv():
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
